9c20c1e84484ffe8ec3d7ca7c1fdd3798aa2e114
Thomas Maxwell <maxxabillion@asatera.com>
2023-09-04 15:23:48 -0400

    functions working

--- twitterbot.py
+++ twitterbot.py
@@ -34,7 +34,6 @@ def fetch_news_articles():
     params = {"limit": 1, "ORG": "Weedmaps"}
     headers = {'Accept': 'application/json', 'apikey': cannabis_news_api_key}
     
-
     try:
         response = requests.get(cannabis_news_endpoint, params=params, headers=headers)
         response.raise_for_status()  # Raise an exception for HTTP error
@@ -56,11 +55,43 @@ def fetch_news_articles():
         
 # Function to generate tweets using OpenAI
 
+def generate_tweet(news_summary):
+    prompt = f"You are an expert on cannabis twitter bot that tweets news articles about cannabis. The data you have access to is a database of over 950k news articles. The fields of each articles are the following: _id, title, url, description, author, publishedAt, content, source_id, urlToImage, GPE, ORG, PERSON, Tokens, Topic, Topic_Contribution, sentiment, and sentiment_score. Using the previous mentioned, create a Twitter Tweet that summarizes the articles, gives a prediction or general commentary. Include the article’s url, mention the article’s sentiment, and Topic. Use a casual tone of a knowledgable cannabis news reporter. Make it a detailed and well thought out tweet. Mention that the articles was retrieved from cannabisnewsapi.ai Constraints: Do not include pretext or context in your response, only return the tweet. Maximum 10,000 characters and use bold and italic text formatting where appropriate.\n\n{news_summary}"
+    
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=100
+    )
+    return response.choices[0].text.strip()
+
+# Function to post a tweet to Twitter
+def post_tweet(tweet_content):
+    try:
+        # Post the tweet using Tweepy
+        twitter_api.update_status(status=tweet_content)
+        print("Tweet posted successfully:", tweet_content)
+    except tweepy.errors.TweepyException as e:
+        print("Error posting tweet:", e)
+
+
 # Main function
 
 def main():
     # Fetch a news article from your database
     fetch_news_articles()
+
+    # Generate a tweet based on the news article
+    news_summary = "Here is a summary of the news article you fetched."
+    tweet = generate_tweet(news_summary)
+
+    # Print the generated tweet
+    print("Generated Tweet:")
+    print(tweet)
+
+    # Post the generated tweet to Twitter
+    post_tweet(tweet)
 
 if __name__ == "__main__":
     main()
