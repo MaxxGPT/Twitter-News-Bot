c3791e9d7b3613154027944bd827266e46ced740
Thomas Maxwell <maxxabillion@asatera.com>
2023-10-15 20:36:47 -0400

    fixing logger.py

--- twitter_news_bot/logger.py
+++ twitter_news_bot/logger.py
@@ -1,28 +1,33 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='application_errors.log', log_level=logging.INFO):
+def setup_logger(log_file='logs/application_errors.log'):
     """
     Configures the logger with specified log file and log level.
     """
     # Create logger
     logger = logging.getLogger()
+    
+    # Determine log level based on the environment
+    log_level = logging.DEBUG if os.getenv('ENV') == 'dev' else logging.INFO
     logger.setLevel(log_level)
 
-    # Create file handler and set level to log_level
-    fh = logging.FileHandler(log_file)
-    fh.setLevel(log_level)
+    # Create rotating file handler and set level to log_level
+    rf_handler = RotatingFileHandler(log_file, maxBytes=1e6, backupCount=3)
+    rf_handler.setLevel(log_level)
 
     # Create console handler with a higher log level
     ch = logging.StreamHandler()
-    ch.setLevel(logging.WARNING)
+    ch.setLevel(logging.ERROR)
 
     # Create formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     # Add formatter to handlers
-    fh.setFormatter(formatter)
+    rf_handler.setFormatter(formatter)
     ch.setFormatter(formatter)
 
     # Add handlers to logger
-    logger.addHandler(fh)
+    logger.addHandler(rf_handler)
     logger.addHandler(ch)
